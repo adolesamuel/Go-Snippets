dde7490439049b70181dac8a532b723746c0368c
Adole Samuel Sani <adolesamuel4@gmail.com>
2023-07-19 09:29:46 +0100

    first commit

---
+++ Create File in Go/create_file.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	// creation of an empty file
+	// using the Create() method
+	myFile, es := os.Create("Heyy.txt")
+	if es != nil {
+		log.Fatal(es)
+	}
+	log.Println(myFile)
+	myFile.Close()
+}
